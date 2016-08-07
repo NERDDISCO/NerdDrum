a7bc6b842382b41df1eb99fd3e7c7c9de29bbea3
TimPietrusky <timpietrusky@gmail.com>
2016-08-07 14:07:33 +0200

    Added "ndSelector".

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,33 @@
-"use strict";
+'use strict';
+/*global process*/
 
 import { ndConnector } from "./lib/ndConnector";
 import { ndMidi } from "./lib/ndMidi";
+import { ndSelector } from "./lib/ndSelector";
 
-const connect = new ndConnector({
+
+
+
+/**
+ * Connect the client to the server over socket.io
+ */
+const websocket = new ndConnector({
   url : 'localhost:1337'
 });
 
+/**
+ * Control MIDI devices.
+ */
 const midi = new ndMidi({
   debug : true
+});
+console.log(process.env.PIXEL_PER_LED_X);
+/*
+ * Select an area on the canvas.
+ */
+const selector_front = new ndSelector({
+  selector_element_name : 'front',
+  selector_element_x : 500,
+  selector_element_width : process.env.PIXEL_PER_LED_X * 8,
+  selector_element_height : process.env.PIXEL_PER_LED_Y * 30
 });
