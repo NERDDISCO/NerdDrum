78917914704817e58a6b89503ea531597148c012
TimPietrusky <timpietrusky@gmail.com>
2016-08-09 09:24:33 +0200

    Imported "config".

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,12 @@
 'use strict';
 /*global process*/
 
+import config from "./config";
 import ndConnector from "./lib/ndConnector";
 import ndMidi from "./lib/ndMidi";
 import ndSelector from "./lib/ndSelector";
 
-
-
-/*
- * Container for the NERD DISCO, you might say it's a party.
- */
+ // Container for the NERD DISCO, you might say it's a party.
 const container = document.getElementById('nerddisco');
 
 
@@ -30,8 +27,6 @@ const midi = new ndMidi({
   debug : true
 });
 
-console.log(process.env.PIXEL_PER_LED_X);
-
 /*
  * Select an area on the canvas.
  */
@@ -39,6 +34,6 @@ const selector_front = new ndSelector({
   parent_element : container,
   selector_element_name : 'front',
   selector_element_x : 500,
-  selector_element_width : process.env.PIXEL_PER_LED_X * 8,
-  selector_element_height : process.env.PIXEL_PER_LED_Y * 30
+  selector_element_width : config.pixel_per_led_x * 8,
+  selector_element_height : config.pixel_per_led_y * 30
 });
