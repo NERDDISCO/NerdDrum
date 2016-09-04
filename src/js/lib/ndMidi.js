58c895e54e18e0b7e0da5fd6af80a2d4b335e2cb
TimPietrusky <timpietrusky@gmail.com>
2016-09-04 07:18:09 +0200

    Fire a CustomEvent for ever MIDI message received.

--- src/js/lib/ndMidi.js
+++ src/js/lib/ndMidi.js
@@ -15,6 +15,9 @@ export default class ndMidi {
     // Show debugging logs?
     this.debug = args.debug || false;
 
+    //
+    this.event = new CustomEvent("ndMidiEvent", { nd : {} });
+/*
     // A list of devices
     this.devices = args.devices || null;
 
@@ -32,7 +35,9 @@ export default class ndMidi {
         this._devices.set(device.name, device);
       }
 
-    }
+    }*/
+
+    this.connect();
 
   } // / constructor
 
@@ -101,7 +106,7 @@ export default class ndMidi {
         console.log("type:", input.type, "| id:", input.id, "| manufacturer:", input.manufacturer, "| name:", input.name, "| version:", input.version);
       }
 
-
+/*
       // The input is a defined device
       if (this._devices.has(input.name)) {
 
@@ -128,6 +133,8 @@ export default class ndMidi {
         } // / Mapping for the current input exists
 
       } // / The input is a defined device
+*/
+
 
     } // / Iterate over all input ports
 
@@ -185,96 +192,18 @@ export default class ndMidi {
       return;
     }
 
-    console.log(message);
+    // Create nd.message
+    this.event.nd = {};
+    this.event.nd.message = message;
+    this.event.nd.command = message.data[0].toString(16).charAt(0);
+    this.event.nd.channel = message.data[0].toString(16).charAt(1);
+    this.event.nd.name = this.getCommandName(this.event.nd.command);
+    this.event.nd.note = message.data[1];
+    this.event.nd.velocity = message.data[2];
+    this.event.nd.type = message.data[0];
 
-
-
-    // The device that was used
-    let device = this._devices.get(message.target.name);
-
-    // The device for the current message does not exist
-    if (device == undefined) {
-      return;
-    }
-
-
-
-    // Input
-    let data = message.data;
-
-    // The current MIDI command
-    let midi_command = data[0].toString(16);
-
-    // Note
-    let note = data[1];
-
-    // Velocity
-    let velocity = data[2];
-
-    // Channel
-    // var channel = data[0] & 0xf;
-    let channel = midi_command.charAt(1);
-
-    // Command
-    let channel_command = midi_command.charAt(0);
-
-    // Type
-    let type = data[0];
-
-
-
-    // Do stuff based on the message type
-    switch (channel_command) {
-
-      // Note Off
-      case '8':
-        this.noteOff({ device: device, note : note, velocity : velocity, channel : channel });
-        break;
-
-      // Note On
-      case '9':
-        this.noteOn({ device: device, note : note, velocity : velocity, channel : channel });
-        break;
-
-      // Aftertouch
-      case 'a':
-        this.aftertouch({ device: device, note : note, velocity : velocity, channel : channel });
-        break;
-
-      // Continuous controller
-      case 'b':
-        this.continuousController({ device: device, note : note, velocity : velocity, channel : channel });
-        break;
-
-      // Patch change
-      case 'c':
-        this.patchChange({ device: device, note : note, velocity : velocity, channel : channel });
-        break;
-
-      // Channel Pressure
-      case 'd':
-        this.channelPressure({ device: device, note : note, velocity : velocity, channel : channel });
-        break;
-
-      // Pitch bend
-      case 'e':
-        this.pitchBend({ device: device, note : note, velocity : velocity, channel : channel });
-        break;
-
-      // (non-musical commands)
-      case 'f':
-        this.nonMusicalCommand({ device: device, note : note, velocity : velocity, type : type });
-        break;
-
-      default:
-        console.log('UNKOWN VALUE', 'channel_command', channel_command, 'channel', channel, 'type', type, 'note', note, 'velocitiy', velocity, 'message', message);
-
-    } // / switch(type)
-
-
-
-    // Send a custom event
-    window.dispatchEvent(new CustomEvent('ndMidi', { 'detail' : { 'id': device.id, 'name': device.name, 'note': device.inputMapping.get(note) } }));
+    // Send nd.message into the world
+    document.body.dispatchEvent(this.event);
 
   } // / ndMidi.inputMessage
 
@@ -282,115 +211,20 @@ export default class ndMidi {
 
 
 
-  getCommand(args) {
-  	return this._devices.get(args.name).inputMapping.get(args.note.note);
+  getCommandName(command) {
+
+    switch (command) {
+      case '8': return 'noteOff';
+      case '9': return 'noteOn';
+      case 'a': return 'aftertouch';
+      case 'b': return 'continuousController';
+      case 'c': return 'patchChange';
+      case 'd': return 'channelPressure';
+      case 'e': return 'pitchBend';
+      case 'f': return 'nonMusicalCommand';
+      default: break;
+    }
+
   }
-
-
-
-
-  /**
-   * Note (for example a button on a drumpad) on MIDI device was activated (for example pressed).
-   *
-   */
-  noteOn(args) {
-    if (this.debug) {
-      console.log('note on', args.note);
-    }
-
-    // Get the current command
-    let command = args.device.inputMapping.get(args.note);
-
-    // Merge the values
-    command = Object.assign(command, args);
-
-    // Set values
-    command.pressed = true;
-    command.noteOn = true;
-    command.noteOff = false;
-
-    // Update the command
-    args.device.inputMapping.set(args.note, command);
-  }
-
-
-
-  /**
-   * Note (for example a button on a drumpad) on MIDI device was activated (for example pressed).
-   *
-   */
-  noteOff(args) {
-    if (this.debug) {
-      console.log('note off', args.note);
-    }
-
-    // Get the current command
-    let command = args.device.inputMapping.get(args.note);
-
-    // Merge the values
-    command = Object.assign(command, args);
-
-    // Set values
-    command.pressed = false;
-    command.noteOn = false;
-    command.noteOff = true;
-
-    // Update the command
-    args.device.inputMapping.set(args.note, command);
-  }
-
-
-
-  pitchBend(args) {
-    if (this.debug) {
-      console.log('pitch bend', args);
-    }
-  }
-
-
-
-  continuousController(args) {
-    if (this.debug) {
-      console.log('continuous controller', args);
-    } else {
-      args.device.inputMapping[args.note] = Object.assign(args.device.inputMapping[args.note], args);
-    }
-  }
-
-
-
-  patchChange(args) {
-    if (this.debug) {
-      console.log('patch Change', args);
-    }
-  } // / ndMidi.patchChange
-
-
-
-  aftertouch(args) {
-    if (this.debug) {
-      console.log('aftertouch', args);
-    }
-  } // / ndMidi.aftertouch
-
-
-
-  channelPressure(args) {
-    if (this.debug) {
-      console.log('channel pressure', args);
-    }
-  } // / ndMidi.channelPressure
-
-
-
-  nonMusicalCommand(args) {
-    if (this.debug) {
-      console.log('(non-musical commands)', args);
-    }
-  } // / ndMidi.nonMusicalCommands
-
-
-
-
 
 } // / ndMidi
